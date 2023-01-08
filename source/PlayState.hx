d9acd792f811fe01436d8975a2ded75edf297be7
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2023-01-07 18:32:28 -0600

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2471,7 +2471,7 @@ class PlayState extends MusicBeatState
 			+ ' | Misses: ' + songMisses 
 			+ ' | Average MS: ?'
 			+ ' | Grade: ' + ratingName
-			+ ' | Health: ' + healthBar.percent;
+			+ ' | Health: 50";
 		} else {
 			scoreTxt.text = 'Score: ' + songScore 
 			+ ' | Misses: ' + songMisses 
