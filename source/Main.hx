251a97f41818b5a0e6ec5954fc377b66bfa8ac2e
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2023-01-25 17:54:18 -0600

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -127,7 +127,7 @@ class Main extends Sprite
 			}
 		}
 
-		errMsg += "\nUnexpected Twist: " + e.error + "\nPlease report this crash to the GitHub page: https://github.com/PDL2CGamejolt/FNFPaytenEditionNEW/issues\n\n> Crash Handler written by: sqirra-rng";
+		errMsg += "\nGame Crash: " + e.error + "\nPlease report this crash to the GitHub page: https://github.com/PDL2CGamejolt/FNFPaytenEditionNEW/issues\nSorry for the inconvinience it has caused during gameplay.\n> Crash Handler written by: sqirra-rng";
 
 		if (!FileSystem.exists("./crash/"))
 			FileSystem.createDirectory("./crash/");
