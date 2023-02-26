05f695b7d3d65ef0a5721cac88e342b20205d43a
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2023-02-26 16:39:26 -0600

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1306,7 +1306,7 @@ class PlayState extends MusicBeatState
 		add(scoreTxt);
 
 		songTxt = new FlxText(12, FlxG.height - 24, 0, "", 8);
-		songTxt.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		songTxt.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.YELLOW, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.RED);
 		songTxt.scrollFactor.set();
 		songTxt.borderSize = 1;
 		if (!ClientPrefs.hideWatermark && !ClientPrefs.hideHud) {
@@ -1315,7 +1315,7 @@ class PlayState extends MusicBeatState
 			songTxt.visible = false;
 		}
 		add(songTxt);
-		songTxt.text = curSong + " (" + storyDifficultyText + ") " + "| Payten Edition v1, built on OS " + MainMenuState.osEngineVersion;
+		songTxt.text = curSong + "| Payten Edition v1, built on OS " + MainMenuState.osEngineVersion;
 
 		botplayTxt = new FlxText(400, timeBarBG.y + 55, FlxG.width - 800, "Hey, I think you need to get tested because I think I might've given you spamophobia...", 32);
 		botplayTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.YELLOW, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.RED);
@@ -3069,10 +3069,10 @@ class PlayState extends MusicBeatState
 
 	override public function update(elapsed:Float)
 	{
-		if (FlxG.keys.justPressed.NINE)
-		{
-			iconP1.swapOldIcon();
-		}
+		///if (FlxG.keys.justPressed.NINE)
+		//{
+			//iconP1.swapOldIcon();
+		//}
 		callOnLuas('onUpdate', [elapsed]);
 		callOnHScripts('update', [elapsed]);
 
@@ -5594,40 +5594,40 @@ class PlayState extends MusicBeatState
 							switch(weekName) //I know this is a lot of duplicated code, but it's easier readable and you can add weeks with different names than the achievement tag
 							{
 								case 'week1':
-									if(achievementName == 'week1_nomiss') unlock = true;
+									if(achievementName == 'week1_nomiss') unlock = false;
 								case 'week2':
-									if(achievementName == 'week2_nomiss') unlock = true;
+									if(achievementName == 'week2_nomiss') unlock = false;
 								case 'week3':
-									if(achievementName == 'week3_nomiss') unlock = true;
+									if(achievementName == 'week3_nomiss') unlock = false;
 								case 'week4':
-									if(achievementName == 'week4_nomiss') unlock = true;
+									if(achievementName == 'week4_nomiss') unlock = false;
 								case 'week5':
-									if(achievementName == 'week5_nomiss') unlock = true;
+									if(achievementName == 'week5_nomiss') unlock = false;
 								case 'week6':
-									if(achievementName == 'week6_nomiss') unlock = true;
+									if(achievementName == 'week6_nomiss') unlock = false;
 								case 'week7':
-									if(achievementName == 'week7_nomiss') unlock = true;
+									if(achievementName == 'week7_nomiss') unlock = false;
 							}
 						}
 					case 'ur_bad':
 						if(ratingPercent < 0.2 && !practiceMode) {
-							unlock = true;
+							unlock = false;
 						}
 					case 'ur_good':
 						if(ratingPercent >= 1 && !usedPractice) {
-							unlock = true;
+							unlock = false;
 						}
 					case 'roadkill_enthusiast':
 						if(Achievements.henchmenDeath >= 100) {
-							unlock = true;
+							unlock = false;
 						}
 					case 'oversinging':
 						if(boyfriend.holdTimer >= 10 && !usedPractice) {
-							unlock = true;
+							unlock = false;
 						}
 					case 'hype':
 						if(!boyfriendIdled && !usedPractice) {
-							unlock = true;
+							unlock = false;
 						}
 					case 'two_keys':
 						if(!usedPractice) {
@@ -5637,16 +5637,16 @@ class PlayState extends MusicBeatState
 							}
 
 							if(howManyPresses <= 2) {
-								unlock = true;
+								unlock = false;
 							}
 						}
 					case 'toastie':
 						if(/*ClientPrefs.framerate <= 60 &&*/ ClientPrefs.lowQuality && !ClientPrefs.globalAntialiasing && !ClientPrefs.imagesPersist) {
-							unlock = true;
+							unlock = false;
 						}
 					case 'debugger':
 						if(Paths.formatToSongPath(SONG.song) == 'test') {
-							unlock = true;
+							unlock = false;
 						}
 				}
 
