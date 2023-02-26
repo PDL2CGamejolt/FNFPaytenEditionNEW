2f7a43b32f5a0f010adaff462f8d8e35e8d2f1a4
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2023-02-26 16:35:17 -0600

    Update GameplayChangersSubstate.hx
    lel

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -69,15 +69,15 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 		var option:GameplayOption = new GameplayOption('Health Gain Multiplier', 'healthgain', 'float', 1);
 		option.scrollSpeed = 2.5;
 		option.minValue = 0;
-		option.maxValue = 5;
+		option.maxValue = 10;
 		option.changeValue = 0.1;
 		option.displayFormat = '%vX';
 		optionsArray.push(option);
 
 		var option:GameplayOption = new GameplayOption('Health Loss Multiplier', 'healthloss', 'float', 1);
 		option.scrollSpeed = 2.5;
-		option.minValue = 0.5;
-		option.maxValue = 5;
+		option.minValue = 0;
+		option.maxValue = 10;
 		option.changeValue = 0.1;
 		option.displayFormat = '%vX';
 		optionsArray.push(option);
